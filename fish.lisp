28aeb661255d0ecbf7dbf175fcd14d2bf120e94b
Christian LaCourt <lanerdofchristian@gmail.com>
2012-09-27 19:03:39 -0400

    LICENSE
    Hmm... Just don't steal it, okay?

---
+++ fish.lisp
@@ -0,0 +1,24 @@
+(defpackage fish)
+(defvar fish::settings (make-hash-table))
+(setf (gethash 'nyanmode fish::settings) nil)
+(defun fish::main () (format t "FISH: Feline Interactive SHell~%>")
+	(do ((i (read-line) (read-line)))
+		((and (> (length i) 0) (equal (subseq (string-downcase i) 0 1) "q"))
+		 (progn (setf (gethash 'nyanmode fish::settings) nil)))
+		(cond
+			((listp (read-from-string i)) (format t "~A~%>" (eval (read-from-string i))))
+			((equal (string-downcase i) "-nyanmode") (progn
+				(cond
+					((gethash 'nyanmode fish::settings) (setf (gethash 'nyanmode fish::settings) nil))
+					((not (gethash 'nyanmode fish::settings)) (setf (gethash 'nyanmode fish::settings) t)))
+				(format t ">")))
+			((gethash 'nyanmode fish::settings) (let ((num (random 4))) (cond
+				((equal num 0) (format t "Nyan!~%>"))
+				((equal num 1) (format t "NYAN!~%>"))
+				((equal num 2) (format t "NYANNYANNYAN!!!~%>"))
+				((equal num 3) (format t "NyAnNyAnnYanNYannYAN!!!!!!!!~%>")))))
+			(t (let ((num (random 4))) (cond
+				((equal num 0) (format t "Meow.~%>"))
+				((equal num 1) (format t "Meow?~%>"))
+				((equal num 2) (format t "Meow!~%>"))
+				((equal num 3) (format t "MEOW!~%>"))))))))
